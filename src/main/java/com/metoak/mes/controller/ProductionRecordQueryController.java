f75c74fc08024a4f4524eb868adec1d145a1acf2
许凯文 <kevinx@xukaiwendeMac-mini.local>
2025-10-13 17:10:42 +0800

    feat:add spc query controller

--- src/main/java/com/metoak/mes/controller/ProductionRecordQueryController.java
+++ src/main/java/com/metoak/mes/controller/ProductionRecordQueryController.java
@@ -31,37 +31,40 @@ public class ProductionRecordQueryController {
     @Operation(summary = "生产记录查询方法1")
 //    @RequestBody @Valid ProductionRecordQueryRequest request
     public Result<List<ProductionRecordDto>> queryMethod1(
-            @RequestParam(defaultValue = "0") int positionOffset,
+//            @RequestParam(defaultValue = "0") int positionOffset,
             @RequestParam(required = false) String[] attrNos,
             @RequestParam(required = false) int origin,
             @RequestParam(required = false) int device,
             @RequestParam(required = false) int station,
             @RequestParam(required = false) int position,
-            @RequestParam(required = false) String timeField,
             @RequestParam(required = false) String startTime,
             @RequestParam(required = false) String endTime
     ) {
 //        Class<?> entityClass = resolveEntityClass(request.getEntityClassName());
-        DatabaseConfig readonlyuser;
+        DatabaseConfig databaseConfig;
         if (origin == 1) {
-            readonlyuser = DatabaseConfig.builder().url("jdbc:mysql://11.11.11.13:3306/mo_mes_db").username("root").password("momeshou").build();
+            databaseConfig = DatabaseConfig.builder().url("jdbc:mysql://11.11.11.13:3306/mo_mes_db").username("root").password("momeshou").build();
         } else {
-            readonlyuser = DatabaseConfig.builder().url("jdbc:mysql://192.168.188.11:3306/mo_mes_db").username("root").password("momeshou").build();
+            databaseConfig = DatabaseConfig.builder().url("jdbc:mysql://192.168.188.11:3306/mo_mes_db").username("root").password("momeshou").build();
         }
         List<ProductionRecordDto> dtos = Collections.emptyList();
+        int positionOffset = 0;
+        if (device == 1 || device == 2) {
+            positionOffset = 1;
+        }
         if (device == 2 || device == 3) {
             dtos = productionRecordQueryService.queryMethod1(
-                    readonlyuser,
+                    databaseConfig,
                     MoAutoAdjustSt08.class,
                     positionOffset,
                     attrNos,
-                    timeField,
+                    "add_time",
                     startTime,
                     endTime
             );
         } else if (device == 1) {
             dtos = productionRecordQueryService.queryMethod2(
-                    readonlyuser,
+                    databaseConfig,
                     positionOffset,
                     attrNos,
                     startTime,
@@ -71,24 +74,24 @@ public class ProductionRecordQueryController {
         return Result.ok(dtos);
     }
 
-    @GetMapping("/st07")
-    @Operation(summary = "生产记录查询方法2（ST07）")
-    public Result<List<ProductionRecordDto>> queryMethod2(
-            @RequestParam(defaultValue = "0") int positionOffset,
+    @GetMapping("spc")
+    @Operation(summary = "SPC")
+//    @RequestBody @Valid ProductionRecordQueryRequest request
+    public Result<List<String>> spc(
             @RequestParam(required = false) String[] attrNos,
+            @RequestParam(required = false) int origin,
+            @RequestParam(required = false) int device,
+            @RequestParam(required = false) int station,
+            @RequestParam(required = false) int position,
+            @RequestParam(required = false) int stage,
+            @RequestParam(required = false) int count,
             @RequestParam(required = false) String startTime,
             @RequestParam(required = false) String endTime
     ) {
-        DatabaseConfig readonlyuser = DatabaseConfig.builder().url("jdbc:mysql://172.24.81.104:3306/mo_mes_db").username("root").password("momeshou").build();
-        List<ProductionRecordDto> dtos = productionRecordQueryService.queryMethod2(
-                readonlyuser,
-                positionOffset,
-                attrNos,
-                startTime,
-                endTime
-        );
+
         return Result.ok(dtos);
     }
+
 
     private Class<?> resolveEntityClass(String className) {
         if (!StringUtils.hasText(className)) {

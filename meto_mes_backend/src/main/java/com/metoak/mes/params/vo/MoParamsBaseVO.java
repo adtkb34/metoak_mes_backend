f891ccab84e75f4c59772a2ac57f9dbe3ab9ab41
xukaiwen <1848507523@qq.com>
2025-12-22 11:48:54 +0800

    .

---
+++ meto_mes_backend/src/main/java/com/metoak/mes/params/vo/MoParamsBaseVO.java
@@ -0,0 +1,39 @@
+package com.metoak.mes.params.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "MoParamsBaseVO", description = "参数集展示对象")
+public class MoParamsBaseVO {
+
+    @ApiModelProperty("参数集类型：0-工程，1-工艺")
+    private Integer type;
+
+    @ApiModelProperty("关联项（工艺 / 工单 / 工序等）")
+    private String relation;
+
+    @ApiModelProperty("参数集名称")
+    private String name;
+
+    @ApiModelProperty("参数集描述")
+    private String description;
+
+    @ApiModelProperty("参数集版本")
+    private String version;
+
+    @ApiModelProperty("创建人")
+    private String createdBy;
+
+    @ApiModelProperty("创建时间")
+    private LocalDateTime createdAt;
+}
